4a892cdb26ec15d29ba475b81a6ce4e9342ebe52
Kakeru <stand.by.we@gmail.com>
2022-02-22 13:27:11 +0900

    ガワ

---
+++ lintPlayground/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  lintPlayground
+//
+//  Created by Kakeru Fukuda on 2022/02/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ lintPlayground/lintPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  lintPlaygroundApp.swift
+//  lintPlayground
+//
+//  Created by Kakeru Fukuda on 2022/02/22.
+//
+
+import SwiftUI
+
+@main
+struct lintPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
